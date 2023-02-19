0cd1333629856274927bf5ecb6e15e9fdf3343ce
anagarcia1207 <garcia.ana5535@gmail.com>
2023-02-19 15:55:44 -0600

    implemented search

--- jokes/views.py
+++ jokes/views.py
@@ -10,6 +10,7 @@ from django.views.generic import (
 
 from .models import Joke, JokeVote
 from .forms import JokeForm
+from django.db.models import Q
 
 class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
@@ -95,6 +96,12 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if 'q' in self.request.GET: # Filter by search query
+            q = self.request.GET.get('q') 
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+            )
 
         if 'slug' in self.kwargs: # Filter by category or tag
             slug = self.kwargs['slug']
