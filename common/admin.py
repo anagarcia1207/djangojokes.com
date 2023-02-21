1fc53df3789e8749612ccbb503369c01aa27b000
anagarcia1207 <garcia.ana5535@gmail.com>
2023-02-20 20:33:40 -0600

    10 per page

--- common/admin.py
+++ common/admin.py
@@ -5,5 +5,5 @@ admin.site.site_title = 'Django Jokes Admin'
 admin.site.site_header = 'Django Jokes Admin'
 
 class DjangoJokesAdmin(admin.ModelAdmin):
-    list_per_page = 25
+    list_per_page = 10
     list_max_show_all = 1000
